70b6eafdcda9840d78627ff5c490b6b331092f39
shumovod <shumovod@mail.ru>
2023-11-29 21:05:09 +0300

    lb6

--- code/lb_6/message_lose.cc
+++ code/lb_6/message_lose.cc
@@ -1,7 +1,7 @@
 #include "headers/message_lose.h"
 
 std::string MessageLose::get_message() {
-    return "Player lose!\nCoordinates: (" + std::to_string(player_.get_x()) + ", " + std::to_string(player_.get_y()) + ")";
+    return "Player lose!\tCoordinates: (" + std::to_string(player_.get_x()) + ", " + std::to_string(player_.get_y()) + ")";
 }
 
 MessageLose::MessageLose(Player& player) : player_(player) {}
--- code/lb_6/message_new_game.cc
+++ code/lb_6/message_new_game.cc
@@ -1,7 +1,7 @@
 #include "headers/message_new_game.h"
 
 std::string MessageNewGame::get_message() {
-    return "A new game has been launched!\nSize: (" + std::to_string(field_.get_width()) + ", " + std::to_string(field_.get_height()) + 
+    return "A new game has been launched!\tSize: (" + std::to_string(field_.get_width()) + ", " + std::to_string(field_.get_height()) + 
            ")  Coordinates start: (" + std::to_string(field_.get_entrance_x()) + ", " + std::to_string(field_.get_entrance_y()) + ")";
 }
 
